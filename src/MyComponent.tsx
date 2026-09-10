59fde3b241e96b3482fbb460befaaf9a4d4cf3b5
beni-niyogisubizo <b.niyogisub@alustudent.com>
2026-09-10 21:54:10 +0200

    Add MyComponent React component

---
+++ src/MyComponent.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const MyComponent: react.FC = () => {
+    return (
+        <div>
+            <h1>Hello from MyComponent!</h1>
+            <p>This is a paragraph of text within my component.</p>
+        </div>
+    );
+};
+
+export default MyComponent;
